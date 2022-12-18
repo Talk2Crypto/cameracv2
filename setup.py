47c6e627f997069a8c4a7b4f9592281c48516fbe
Farsana F J <119430344+Talk2Crypto@users.noreply.github.com>
2022-12-18 11:56:22 +0530

    This is My package 📦. That help people who is working with cv2 it is is a camera you can just import it and create a camera 📸

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='cameracv2',
+ version='0.1',
+ author="Muhammed Yusuf",
+ author_email="farsanafarooq@gmail.com",
+ description="This tool was made so that if you are working with cv2 and you want to use the camera you does not need to write that much code just import this pac",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ]
+)
